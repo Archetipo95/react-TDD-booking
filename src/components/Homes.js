38d506e61df1dda1b26c3997ecfedbb2c65f106d
Archetipo95 <martin.masevski@gmail.com>
2021-06-05 15:22:17 +0200

    Implemented homes + testing

--- src/components/Homes.js
+++ src/components/Homes.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import apiClient from "./../services/apiClient";
 
-const Homes = () => {
+export default function Homes() {
+  const [homesState, setHomesState] = useState([]);
+
+  useEffect(() => {
+    const homesDataPromise = apiClient.getHomes();
+
+    homesDataPromise.then(
+      (homesData) => console.log(homesData) || setHomesState(homesData)
+    );
+  }, []);
+
+  let homes;
+
+  homes = homesState.map((home, index) => {
     return (
-        <div>
-            Homes
+      <div className="col-6 col-md-6 col-lg-4 col-xl-3 mb-3">
+        <div data-testid="home" key={index} className="card w-100">
+          <img
+            data-testid="home-image"
+            src={home.image}
+            alt={home.title}
+            className="card-img-top"
+          />
+          <div className="card-body">
+            <div data-testid="home-title" className="card-title h5">{home.title}</div>
+            <div data-testid="home-price">${home.price}/night</div>
+            <div data-testid="home-location">{home.location}</div>
+          </div>
         </div>
-    )
-}
+      </div>
+    );
+  });
 
-export default Homes
+  return (
+    <div className="container m-2">
+      <h1>Homes</h1>
+      <div className="row">{homes}</div>
+    </div>
+  );
+}
---
+++ src/components/Homes.test.js
@@ -0,0 +1,69 @@
+import {
+  act,
+  render,
+  getAllByTestId,
+  getNodeText,
+} from "@testing-library/react";
+import React from "react";
+import Homes from "./Homes";
+import apiClient from "./../services/apiClient";
+
+let container = null;
+
+beforeEach(async () => {
+  jest.spyOn(apiClient, "getHomes").mockImplementation(async () => {
+    return Promise.resolve([
+      {
+        title: "Test home 1",
+        image: "listing.jpg",
+        location: "Test location 1",
+        price: "1",
+      },
+      {
+        title: "Test home 2",
+        image: "listing.jpg",
+        location: "Test location 2",
+        price: "2",
+      },
+      {
+        title: "Test home 3",
+        image: "listing.jpg",
+        location: "Test location 3",
+        price: "3",
+      },
+    ]);
+  });
+  container = render(<Homes />).container;
+
+  await act(async () => {});
+});
+
+it("should show the home", () => {
+  const homes = getAllByTestId(container, "home");
+
+  expect(homes.length).toBeGreaterThan(0);
+});
+
+it("should show the title", () => {
+  const homeTitles = getAllByTestId(container, "home-title");
+
+  expect(getNodeText(homeTitles[0])).toBe("Test home 1");
+});
+
+it("should show the image", () => {
+  const homeImages = getAllByTestId(container, "home-image");
+
+  expect(homeImages[0]).toBeTruthy();
+});
+
+it("should show the location", () => {
+  const homeLocations = getAllByTestId(container, "home-location");
+
+  expect(getNodeText(homeLocations[0])).toBe("Test location 1");
+});
+
+it("should show the price", () => {
+  const homePrices = getAllByTestId(container, "home-price");
+
+  expect(getNodeText(homePrices[0])).toBe("$1/night");
+});
