ce492300902c30c76103c6f84189683ea281e758
Archetipo95 <martin.masevski@gmail.com>
2021-06-05 11:10:07 +0200

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Homes from "./components/Homes";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Homes />
+    </>
+  );
+}
+
+export default App;
